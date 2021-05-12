5befe74f9f9b849fbd0ff56e55e3deb3cc4b7a17
Olim-Jabborov <olimjonjabborov0701@gmail.com>
2021-05-12 12:46:54 +0500

    Online shop

---
+++ shop/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('<slug:category_slug>', views.home, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>', views.product, name="product_detail"),
+]
---
+++ shop/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+# from django.http import HttpResponse
+
+# Create your views here.
+def home(request, category_slug=None):
+    category_page = None
+    products = None
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category_page, available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render(request, 'home.html', {'category': category_page, 'products': products})
+
+def product(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'product.html', {'product': product})
